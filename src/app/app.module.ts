1fd1f49db282b86435e4083db000cc3be1d43698
barryroubaix <62664220+barryroubaix@users.noreply.github.com>
2023-06-05 14:54:56 -0400

    setup and testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
-import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AuthModule } from '@auth0/auth0-angular';
-import { environment as env } from '../environments/environment';
-import { AppRoutingModule } from './app-routing.module';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AuthHttpInterceptor, AuthModule } from '@auth0/auth0-angular';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { SharedModule } from './shared';
+import { environment as env } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,7 +17,20 @@ import { SharedModule } from './shared';
     HttpClientModule,
     AuthModule.forRoot({
       ...env.auth0,
+      httpInterceptor: {
+        allowedList: [
+            `${env.api.serverUrl}/api/messages/admin`,
+            `${env.api.serverUrl}/api/messages/protected`
+        ],
+      },
     }),
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthHttpInterceptor,
+      multi: true,
+    },
   ],
   bootstrap: [AppComponent],
 })
